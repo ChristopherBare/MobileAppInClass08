6b74fdba40b0f8022d4205e42b2b9a7a242790bf
Christopher Bare <christopherbare@outlook.com>
2018-07-18 18:22:39 -0400

    I have no idea what I'm doing.

--- app/src/main/java/com/christopherbare/inclass08/MainActivity.java
+++ app/src/main/java/com/christopherbare/inclass08/MainActivity.java
@@ -2,12 +2,32 @@ package com.christopherbare.inclass08;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        button = findViewById(R.id.buttonCreate);
+        listView = findViewById(R.id.listView);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewContactFragment fragment = (NewContactFragment) getFragmentManager().findFragmentById(R.layout.fragment_new_contact);
+            }
+        });
+
+
     }
 }
